03848391773c32cfc0e25bdade9f4cfc4ca71476
diwasneupane <diwas.neupane55@gmail.com>
2024-04-23 02:45:15 +0545

    route cofiguration

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,12 +9,13 @@ import {
   getPendingApprovalRequests,
   logoutUser,
 } from "../controllers/user.controller.js";
+import { authenticateToken } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.post("/login", loginUser);
-router.post("/logout", logoutUser);
+router.post("/logout", authenticateToken, logoutUser);
 router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
 
